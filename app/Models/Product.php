7c7b65278e258e77b1b01fac943ee845df0dfe23
Yazan-alsalloum <121229781+Yazan-alsalloum@users.noreply.github.com>
2023-10-13 10:09:05 +0200

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
     public function types()
     {
     	return $this->hasMany(Type::class);
     }
     public function getPriceAttribute($value)
     {
-        $discount = $value*($this->discount/100);//Kortingin euro's
-        $final_price =$value - $discount;//Haal korting af van prijs
-        return number_format($final_price,2);//Zorg altijd voor 2 decimalen
+        $discount = $value*($this->discount/100);
+        $final_price =$value - $discount;
+        return number_format($final_price,2);
     }
 }
